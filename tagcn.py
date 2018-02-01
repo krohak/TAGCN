a8e61a3ff98a5a5cb7388c5a75afb4167e2c1540
krohak <krohak@hku.hk>
2018-02-01 20:11:22 +0800

    working now

--- tagcn.py
+++ tagcn.py
@@ -42,16 +42,18 @@ reset_default_graph()
 f = tf.placeholder(tf.float32, [Nl, Cl])
 path_weight_matrix = tf.placeholder(tf.float32, [Nl, Nl, 2])
 output = tf.placeholder(tf.float32, [None])
-with tf.variable_scope( name + '_vars',reuse=True):
+
+
+with tf.variable_scope( name + '_vars'):
     for i in range(features.shape[0]):
         conv = tf.get_variable("conv", shape=[1,Fl])
         for k in range(Kl):
             for c in range(Cl):
-                with tf.variable_scope("conv1",reuse=None):
                     var_gs = tf.get_variable(name='var_gs',initializer=tf.contrib.layers.xavier_initializer(),shape=[1,Fl])
                     w_k = path_weight_matrix[i,:,k]
                     x_c = f[0:,c]
                     s = tf.matmul(tf.transpose(tf.expand_dims(w_k, 1)),tf.expand_dims(x_c, 1))
                     conv = tf.add(conv,tf.multiply(s[0,0],var_gs))
+		    tf.get_variable_scope().reuse_variables()
         print(conv)
 
